e3ce350e1ff35bcb2ff9ad04847a3d407d9c1d0c
Slava Hromoy <slavachromoy@gmail.com>
2015-07-08 17:41:45 +0300

    last commit.for now

--- advanced-js.js
+++ advanced-js.js
@@ -53,6 +53,7 @@ typeof 1 //"number"
 
 
 //------------primitive as objects------------
+
 "hello world".length // 11
 (12.342).toFixed(2); //"12.34"
 
@@ -81,3 +82,72 @@ Actions.prototype = human1; //inherit one more time
 var runingPerson = new Actions();
 
 console.log(runingPerson.actions); // "Slava Hromoy is runing"
+
+
+//------------Classical inheritance------------
+
+function Parent(name,age){
+this.firstName = name;
+this.age = age;
+}
+Parent.prototype.getPerson = function(){
+	return 'Person: '+ this.firstName + '; Age: ' + this.age + ';';
+};
+
+function Child(name,age){
+	this.firstName = name;
+	this.age = age;
+}
+inherit(Child,Parent);
+
+// 1-st option
+function inherit(C,P){
+	C.prototype = new P();
+}
+//2-nd option 
+function inherit(C,P){
+	var F = function(){};
+	F.prototype = P.prototype;
+	C.prototype = new F();
+	C.uber = P.prototype;
+	C.prototype.constructor = C;
+}
+
+var father = new Parent("oleg",50);
+var son = new Child("Slava",23);
+
+console.log(father.getPerson()); //"Person: oleg; Age: 50;"
+console.log(son.getPerson()); // "Person: Slava; Age: 23;"
+
+
+//------------Clothures------------
+
+function myFunc(x){
+	return function(y){
+		return x+y;
+	};
+}
+myFunc(3)(4); // 7
+
+// another exmpl
+function myFunc() {
+	var x = 21;
+	return function y() {
+		var x = 'hi';
+		console.log(x); // 'hi'
+	};
+}
+
+
+//------------hoisting------------
+
+myname = "global"; 
+function func() { 
+    console.log(myname); 
+    var myname = "local"; 
+    console.log(myname); 
+} 
+func(); 
+// undefined
+// local
+
